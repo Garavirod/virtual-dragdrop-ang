d196837efccd42a7684f0c0ea396bb22689ecc52
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-12-17 14:00:15 -0600

    Virtual scroll with begining middle and end functionalities

--- src/app/components/virtual/virtual.component.ts
+++ src/app/components/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-virtual',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  /* Catch a reference of any element of html document */
+  @ViewChild( CdkVirtualScrollViewport ) myviewport: CdkVirtualScrollViewport;
+
   persons = Array(500).fill(0);
 
   constructor() { }
+
+
+  goEnd(){
+    this.myviewport.scrollToIndex( this.persons.length );
+
+  }
+
+  goBegining(){
+    this.myviewport.scrollToIndex( 0 );
+
+  }
+
+  goInMiddle(){
+    this.myviewport.scrollToIndex( this.persons.length/2 );
+  }
 
   ngOnInit(): void {
   }
